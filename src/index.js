3231f26bc6867ff499da83f04fa82c18d009f172
Earl Davis <edavis67@dxc.com>
2020-12-14 11:42:27 -0600

    Updates

--- src/index.js
+++ src/index.js
@@ -3,8 +3,49 @@ const { ApolloServer, gql } = require('apollo-server-express');
 const port = process.env.PORT || 4000;
 
 const app = express();
-const typeDefs = gql`type Query { hello: String }`;
-const resolvers = { Query: { hello: () => 'Hello World!' } };
+const typeDefs = gql`
+    type Mutation {
+        newNote(content: String!): Note!
+    }
+
+    type Note {
+        id: ID!
+        content: String!
+        author: String!
+    }
+    
+    type Query { 
+        hello: String
+        notes: [Note!]!
+        note(id: ID!): Note!
+    }
+    `;
+const resolvers = { 
+    Query: { 
+        hello: () => 'Hello World!',
+        notes: () => notes,
+        note: (parent, args) => {
+            return notes.find(note => note.id === args.id);
+        }
+    },
+    Mutation: {
+        newNote: (parent, args) => {
+            let noteValue = {
+                id: String(notes.length + 1),
+                content: args.content,
+                author: 'Adam Scott'
+            };
+        notes.push(noteValue);
+        return noteValue;
+        }
+    }
+};
+
+let notes = [
+    { id: '1', content: 'This is a note', author: 'Adam Scott' },
+    { id: '2', content: 'This is another note', author: 'Harlow Everly' },
+    { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' }
+];
 
 const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({ app, path: '/api'});
