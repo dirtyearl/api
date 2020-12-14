c768c2b973c0fe6f21e18158a9f2e7e94feac532
Earl Davis <edavis67@dxc.com>
2020-12-14 10:58:00 -0600

    Chapter 4 Changes

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-// index.js
-// This is the main entry point of our application
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+const port = process.env.PORT || 4000;
+
+const app = express();
+const typeDefs = gql`type Query { hello: String }`;
+const resolvers = { Query: { hello: () => 'Hello World!' } };
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app, path: '/api'});
+
+app.listen({ port }, () =>
+    console.log(
+        `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+    )
+);
